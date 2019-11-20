d33d6f0a6772a60f59521154af4ae3213133a9d3
Lucas Lamar <lucass.lamar@gmail.com>
2019-11-20 11:51:19 -0300

    remove tecnologias de uma lista

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -16,21 +16,27 @@ import React, { Component } from 'react';
     this.setState({ newTech: e.target.value });
   }
   
-  handleSubimit = e => {
+  handleSubmit = e => {
     e.preventDefault();
 
     this.setState({ techs: [...this.state.techs, this.state.newTech],
       newTech: ''
     });
-    
+  }
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
 
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubimit}>
+      <form onSubmit={this.handleSubmit}>
       <ul>
-        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+        {this.state.techs.map(tech => 
+        <li key={tech}>
+        {tech} 
+        <button onClick={() => this.handleDelete(tech)} type="button">Remover</button></li>)}
       </ul>
       <input type="text" onChange={this.handleInputChange} value={this.state.newTech}/>
       <button type="submit">Adicionar</button>
