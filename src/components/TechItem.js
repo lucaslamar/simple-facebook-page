19ea32021cc9f4e8967f71c1fa63812fa384345f
Lucas Lamar <lucass.lamar@gmail.com>
2019-11-21 12:41:36 -0300

    Default Props & PropTypes

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function TechItem({ tech, onDelete }) {
+function TechItem({ tech , onDelete }) {
   return  (
     <li key={tech}>
     {tech} 
     <button onClick={onDelete} type="button">Remover</button>
     </li>
   );
+
+}
+
+TechItem.defaultProps = {
+  tech : 'Defualt',
+}
+
+TechItem.propTypes = {
+  tech : PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
 }
 export default TechItem;
